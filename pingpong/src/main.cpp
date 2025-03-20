2dd56b4891d0bf2814b8b7cc2866c73a140a1be7
meowmeowahr <meowmeowahr@gmail.com>
2025-03-19 23:37:31 -0500

    add ping pong code

--- pingpong/src/main.cpp
+++ pingpong/src/main.cpp
@@ -1,8 +1,12 @@
 #include <Arduino.h>
 
-
-void setup() {
-}
+void setup() { Serial.begin(9600); }
 
 void loop() {
+  if (Serial.available()) {
+    String incoming = Serial.readStringUntil('\n');
+    if (incoming == "ping") {
+      Serial.print("pong\n");
+    }
+  }
 }
